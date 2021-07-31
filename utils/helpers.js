8ee2c3a4e62e2a921bad21b587ff9f098598ece1
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-07-30 20:44:04 -0700

    Updating date helper.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,13 @@
+// module.exports = {
+//   format_date: (date) => {
+//     let Date = date.toLocaleString([], {dateStyle: 'short'});
+//     let Time = date.toLocaleString([], {timeStyle: 'short'});
+//     return `${Date} at ${Time}`;
+//   },
+// };
+
 module.exports = {
-  format_date: (date) => {
-    let Date = date.toLocaleString([], {dateStyle: 'short'});
-    let Time = date.toLocaleString([], {timeStyle: 'short'});
-    return `${Date} at ${Time}`;
-    // return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at ${new Date(date).getHours()}:${new Date(date).getMinutes()} ${new Date(date).getMinutes()}`;
-  },
+  format_date: date => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  }
 };
-
-// toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-
-// {timeStyle: 'short'}
-// return date.toLocaleString([], { hour12: true});
